a6c03f9dd0f3041dcb156164ddf8a1e8515b750e
Azize Zinadinova <azizeshka87@gmail.ru>
2021-11-13 01:06:04 +0300

    исправление ошибок

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -4,8 +4,8 @@ public class Smartphone extends Product {
     private String manufacturer;
 
 
-    public Smartphone(int id, String tatle, int price, String manufacturer) {
-        super(id, tatle, price);
+    public Smartphone(int id, String title, int price, String manufacturer) {
+        super(id, title, price);
         this.manufacturer = manufacturer;
 
     }
